400f451a29889a4446214e224b563f3f25c1e37c
isboco <isboco1@gmail.com>
2024-03-07 18:16:23 +0100

    github action

--- src/main/java/com/presence/testpresence/config/SecurityConfiguration.java
+++ src/main/java/com/presence/testpresence/config/SecurityConfiguration.java
@@ -14,7 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/swagger-ui/**", "/device/**").permitAll()
+                .antMatchers("/presence/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
