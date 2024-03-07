422e4d09074cc7754480d51f9fc6d3691215711c
isboco <isboco1@gmail.com>
2024-03-07 17:49:27 +0100

    github action

--- src/main/java/com/presence/testpresence/controllers/UserController.java
+++ src/main/java/com/presence/testpresence/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
 @RequestMapping("v1.0/user")
 public class UserController {
 
