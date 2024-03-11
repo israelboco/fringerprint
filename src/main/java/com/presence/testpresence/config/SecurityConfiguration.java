cb056c369959bc8896106c7439898f4ff645a91b
isboco <isboco1@gmail.com>
2024-03-11 18:08:38 +0100

    github action

--- src/main/java/com/presence/testpresence/config/SecurityConfiguration.java
+++ src/main/java/com/presence/testpresence/config/SecurityConfiguration.java
@@ -1,35 +1,52 @@
-package com.presence.testpresence.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/swagger-ui/**", "/device/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("isboco")
-                .password(passwordEncoder().encode("isboco"))
-                .authorities("ADMIN");
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package com.presence.testpresence.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/swagger-ui/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .authorizeRequests().antMatchers("/text").permitAll()
+//                .and()
+//                .authorizeRequests().antMatchers("/greeting", "text/**").authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/api/v1.0").permitAll()
+//                .and()
+//                .authorizeHttpRequests().requestMatchers("api/v1.0/**").authenticated()
+//                .and().formLogin()
+//                .and().build();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("fingprint")
+//                .password(passwordEncoder().encode("fingprint"))
+//                .authorities("ADMIN");
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
