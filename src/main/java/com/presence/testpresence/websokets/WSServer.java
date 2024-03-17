d579e5a5e5140e4b32efdddfa2528b6553eafb4a
isboco <isboco1@gmail.com>
2024-03-17 22:24:50 +0100

    isboco

--- src/main/java/com/presence/testpresence/websokets/WSServer.java
+++ src/main/java/com/presence/testpresence/websokets/WSServer.java
@@ -1,8 +1,5 @@
 //package com.presence.testpresence.websokets;
 //
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
 //import java.net.InetSocketAddress;
 //import java.net.UnknownHostException;
 //import java.text.SimpleDateFormat;
--- src/main/java/com/presence/testpresence/websokets/WebSocketPool.java
+++ src/main/java/com/presence/testpresence/websokets/WebSocketPool.java
@@ -1,5 +1,6 @@
 package com.presence.testpresence.websokets;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -7,28 +8,29 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.presence.testpresence.ws.DeviceStatus;
+import org.java_websocket.WebSocket;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-//import org.java_websocket.WebSocket;
 
 
 public class WebSocketPool {
 	
 	
-	public static  final Map<String, DeviceStatus> wsDevice = new HashMap<String, DeviceStatus>();
+	public static  final Map<String, DeviceStatus>wsDevice=new HashMap<String, DeviceStatus>();
 
 
 
 
-	/* Socket avec état */
+  /*  带状态的socket*/
     public static WebSocketSession getDeviceSocketBySn(String deviceSn) {
-    	DeviceStatus deviceStatus = wsDevice.get(deviceSn);
+    	DeviceStatus deviceStatus=wsDevice.get(deviceSn);
     
 		return deviceStatus.getWebSocket();
 		
 	}
 
 
-	/* Ajouter une connexion avec le statut
+    /*添加含状态的连接
     */
     
     public static void addDeviceAndStatus(String deviceSn,DeviceStatus deviceStatus) {
@@ -36,17 +38,17 @@ public class WebSocketPool {
 	}
 
 
-	/* Envoi de données à un seul utilisateur avec statut */
-    public static void sendMessageToDeviceStatus(String sn,String message) {
+  /*  向带状态的用户单个用户发送数据*/
+    public static void sendMessageToDeviceStatus(String sn,String message) throws IOException {
 		DeviceStatus deviceStatus=wsDevice.get(sn);
 		WebSocketSession conn=deviceStatus.getWebSocket();
 			if(null!=conn){				
-				conn.send(message);	
+				conn.sendMessage(new TextMessage(message));
 			}			
 	}
 
 
-	/* delete l'equipement avec le statut */
+   /*移除带状态的设备*/
     public static boolean removeDeviceStatus(String sn) {
 		if(wsDevice.containsKey(sn)){
 			wsDevice.remove(sn);
@@ -84,25 +86,23 @@ public class WebSocketPool {
 		return null;
 	}
 
-	/* Status du device */
+    /*判断状态*/
   public static DeviceStatus getDeviceStatus(String sn) {
 	//  System.out.println(wsDevice.get);
 	return wsDevice.get(sn);
 	
 }
     
-    
-    
-    
+
    /* 发送*/
-    public static void sendMessageToAllDeviceFree(String message) {
+    public static void sendMessageToAllDeviceFree(String message) throws IOException {
 	   System.out.println("空闲发送数据");
 	   Collection<DeviceStatus>deviceStatus=wsDevice.values();
 	   
 	   synchronized (deviceStatus) {
 		for (DeviceStatus deviceStatus2:deviceStatus) {
 			if (deviceStatus2.getWebSocket()!=null) {
-				deviceStatus2.getWebSocket().send(message);
+				deviceStatus2.getWebSocket().sendMessage(new TextMessage(message));
 			}
 		}
 	}
