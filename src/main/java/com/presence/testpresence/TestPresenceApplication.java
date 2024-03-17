a16908169d075b38d4c4edb5c543a1ee97e51715
isboco <isboco1@gmail.com>
2024-03-17 22:32:19 +0100

    isboco

--- src/main/java/com/presence/testpresence/TestPresenceApplication.java
+++ src/main/java/com/presence/testpresence/TestPresenceApplication.java
@@ -1,7 +1,7 @@
 package com.presence.testpresence;
 
 
-import com.presence.testpresence.websokets.WSServer;
+//import com.presence.testpresence.websokets.WSServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
