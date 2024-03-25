7112b86ab2c89d37e397e750a8f3238bc8ebf9ac
isboco <isboco1@gmail.com>
2024-03-25 20:22:23 +0100

    isboco

--- src/main/java/com/presence/testpresence/controllers/TypeMachineController.java
+++ src/main/java/com/presence/testpresence/controllers/TypeMachineController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1.0/type-machine")
+@RequestMapping("api/v1.0/typemachine")
 public class TypeMachineController {
 
     @Autowired
