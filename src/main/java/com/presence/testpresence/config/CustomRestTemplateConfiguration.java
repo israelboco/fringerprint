a595215b255322fd3583869d190baeae970fb15b
isboco <isboco1@gmail.com>
2024-03-28 17:00:31 +0100

    isboco

--- src/main/java/com/presence/testpresence/config/CustomRestTemplateConfiguration.java
+++ src/main/java/com/presence/testpresence/config/CustomRestTemplateConfiguration.java
@@ -1,6 +1,11 @@
 //package com.presence.testpresence.config;
 //
 //import jdk.internal.loader.Resource;
+//import org.apache.http.conn.HttpClientConnectionManager;
+//import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+//import org.apache.http.impl.client.CloseableHttpClient;
+//import org.apache.http.impl.client.HttpClients;
+//import org.apache.http.ssl.SSLContextBuilder;
 //import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
