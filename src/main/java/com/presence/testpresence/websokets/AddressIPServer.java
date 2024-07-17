110c1e41b2ef9e2c3f88df3ea66793226a36c1f1
BOCO Israel <isboco1@gmail.com>
2024-07-17 13:21:32 +0100

    isboco

--- src/main/java/com/presence/testpresence/websokets/AddressIPServer.java
+++ src/main/java/com/presence/testpresence/websokets/AddressIPServer.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
-//import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
