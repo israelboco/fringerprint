f3def79467922e0d0e8e08d673207d26f753ced7
BOCO Israel <isboco1@gmail.com>
2024-07-17 13:41:57 +0100

    isboco

--- src/main/java/com/presence/testpresence/websokets/SendOrderJob.java
+++ src/main/java/com/presence/testpresence/websokets/SendOrderJob.java
@@ -53,7 +53,7 @@ public class SendOrderJob extends Thread{
 			    	List<MachineCommand>pendingCommand=machineCommandRepository.findBySendStatusAndSerial(1, entry.getKey());
 			    	if (pendingCommand.size()<=0) {
 			    		
-			    		entry.getValue().getWebSocket().sendMessage(new TextMessage(inSending.get(0).getContent()));
+			    		entry.getValue().getWebSocket().send(inSending.get(0).getContent());
 						MachineCommand machineCommand = new MachineCommand();
 						machineCommand.setStatus(0);
 						machineCommand.setSendStatus(1);
@@ -72,7 +72,7 @@ public class SendOrderJob extends Thread{
 							machineCommandRepository.save(machineCommand);
 					    	Device device=deviceRepository.findBySerialNum(pendingCommand.get(0).getSerial());
 							if (device.getStatus()!=0) {
-								entry.getValue().getWebSocket().sendMessage(new TextMessage(pendingCommand.get(0).getContent()));
+								entry.getValue().getWebSocket().send(pendingCommand.get(0).getContent());
 								
 							}
 							}else {
