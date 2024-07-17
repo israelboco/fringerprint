6fef422827eb4539beb6521755fa821191c45b01
BOCO Israel <isboco1@gmail.com>
2024-07-17 13:49:54 +0100

    isboco

--- src/main/java/com/presence/testpresence/TestPresenceApplication.java
+++ src/main/java/com/presence/testpresence/TestPresenceApplication.java
@@ -4,7 +4,7 @@ import com.presence.testpresence.websokets.WSServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -17,8 +17,7 @@ import static com.presence.testpresence.websokets.Text.getWsServer;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.presence.testpresence.*"
-},
-exclude = SecurityAutoConfiguration.class)
+})
 //@EnableWebSocket
 @Configuration
 @EnableAsync
