fa25fe705c2bf3157539f599b767072b1ffb637f
BOCO Israel <isboco1@gmail.com>
2024-07-19 11:02:58 +0100

    isboco

--- src/main/java/com/presence/testpresence/config/AdditionalPortConfig.java
+++ src/main/java/com/presence/testpresence/config/AdditionalPortConfig.java
@@ -1,23 +1,23 @@
-package com.presence.testpresence.config;
-
-import org.apache.catalina.connector.Connector;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AdditionalPortConfig {
-
-    @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
-        return factory -> factory.addAdditionalTomcatConnectors(createStandardConnector());
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(7788); // second port
-        return connector;
-    }
-}
-
+//package com.presence.testpresence.config;
+//
+//import org.apache.catalina.connector.Connector;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class AdditionalPortConfig {
+//
+//    @Bean
+//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+//        return factory -> factory.addAdditionalTomcatConnectors(createStandardConnector());
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(7788); // second port
+//        return connector;
+//    }
+//}
+//
